32e909ef4437dffaaab5455813f64437df80a66f
Qinbo-Sun <704069308@qq.com>
2018-11-17 17:18:02 +0800

    Update: IP

--- python/mcapClient/NatNetClient.py
+++ python/mcapClient/NatNetClient.py
@@ -34,9 +34,9 @@ class NatNetClient:
         self.serverIPAddress = "192.168.0.124" 
 
         # Change this value to the IP address of your local network interface
-        #self.localIPAddress = "192.168.0.104" # STAr_Server
-        self.localIPAddress = "192.168.0.106" # Motive
-        #self.localIPAddress = "192.168.0.124"
+        self.localIPAddress = "192.168.0.104" # STAr_Server
+        #self.localIPAddress = "192.168.0.106" # Sailboat02
+        #self.localIPAddress = "192.168.0.124" # Motive
 
         # This should match the multicast address listed in Motive's streaming settings.
         self.multicastAddress = "239.255.42.99"
