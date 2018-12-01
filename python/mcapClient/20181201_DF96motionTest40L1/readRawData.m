bb1cbd45df13fc12697c33c5c64c5827079da3c8
Qinbo-Sun <704069308@qq.com>
2018-12-02 04:19:48 +0800

    Update: Df code

--- python/mcapClient/20181201_DF96motionTest40L1/readRawData.m
+++ python/mcapClient/20181201_DF96motionTest40L1/readRawData.m
@@ -1,9 +1,9 @@
 clc;clear
 str={'D40L1S70','D40L1S75','D40L1S80','D40L1S85','D40L1S90','D40L1S100','D40L1S110','D40L1S120','D40L1S130'} ;
+num=[70,75,80,85,90,100,110,120,130];
 for i=1:length(str)
-    j=i*10+20;
-    temp=importfile(['comSailboat_mcap_IC_motionTest_S',mat2str(j),'.txt']);
+    temp=importfile(['comSailboat_mcap_IC _DF_motionTest_S',mat2str(num(i)),'.txt']);
     eval([cell2mat(str(i)) '=' mat2str(temp)]);
 end
-clearvars i j str temp
+clearvars i j str temp num
 save D40L1
